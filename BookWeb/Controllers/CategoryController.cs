9f241a9f7bd99966d5f00a5eec24039be78c3b5f
Elmedin Llumnica <elmedinllumnica@gmail.com>
2025-12-11 10:31:15 +0100

    Add Category module UI and validation
    - Design Category List page
    - Create Category UI
    - Implement Input Tag Helpers
    - Add Create Category functionality
    - Add server-side validations
    - Add custom validations
    - Add ASP.NET validation summary
    - Enable client-side validation
    - Apply Bootswatch theme and Bootstrap icons

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -19,5 +19,30 @@ namespace BookWeb.Controllers
        List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError
+             ("displayorder","The Display Order cannot match the name");
+            }
+        
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+
+            }
+            return View();
+        }
     }
 }
