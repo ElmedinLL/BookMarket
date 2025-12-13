39abbcee82095cc5f97eda75f78fac43ea8c3f37
Elmedin Llumnica <elmedinllumnica@gmail.com>
2025-12-13 16:02:31 +0100

    feat: Add generic repository pattern
    - Create IRepository<T> interface with CRUD operations
    - Implement Repository<T> base class
    - Add Category-specific repository interface
    - Implement CategoryRepository class

---
+++ Book.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Book.DataAccess.Repository.IRepository;
+using BookMarket.DataAccess.Data;
+using BookMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.DataAccess.Repository
+{
+    public class CategoryRepository : 
+    Repository<Category>, ICateogoryRepository
+    {
+
+        private readonly ApplicationDBContext _db;
+        public CategoryRepository(ApplicationDBContext db) 
+            : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Category obj)
+        {
+           _db.Categories.Update(obj);
+        }
+
+    }
+}
---
+++ Book.DataAccess/Repository/IRepository/ICateogoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.DataAccess.Repository.IRepository
+{
+    public interface ICateogoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+     
+    }
+}
