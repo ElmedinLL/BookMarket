8218ea85f332ac2c4348b8e53429e7589ada67a3
Elmedin Llumnica <elmedinllumnica@gmail.com>
2025-12-18 17:38:46 +0100

    Extending IdentityUser Table

--- Book.DataAccess/Data/ApplicationDBContext.cs
+++ Book.DataAccess/Data/ApplicationDBContext.cs
@@ -18,6 +18,7 @@ namespace BookMarket.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
