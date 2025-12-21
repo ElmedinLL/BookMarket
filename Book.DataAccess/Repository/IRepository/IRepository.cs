f9c8e5e7d23fe861786012b09316bb4319e4ea03
Elmedin Llumnica <elmedinllumnica@gmail.com>
2025-12-21 10:48:51 +0100

    feat(Cart): added Get ShoppingCart

--- Book.DataAccess/Repository/IRepository/IRepository.cs
+++ Book.DataAccess/Repository/IRepository/IRepository.cs
@@ -12,7 +12,7 @@ namespace Book.DataAccess.Repository.IRepository
     {
         // T - Category class
 
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
         T Get(Expression<Func<T,bool>>filter, string? includeProperties = null, bool tracked = false);
         void Add(T entity);
         void Remove(T entity);
